9eba1bd1ce3b0d866b0b8ca537a94dbf5472c1ac
Jeremy Liss <jeremyliss@gmail.com>
2017-05-16 15:43:56 +1200

    initial react setup

---
+++ components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Heading from './Heading'
+
+const App = () => (
+  <div className="homepage">
+    <Heading />
+  </div>
+)
+
+export default App
---
+++ components/Heading.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Heading = () => (
+  <div className="container">
+    <div className="row">
+      <div className="heading">
+        <h1>Jeremy Liss</h1>
+        <p>
+          Enspiral Dev Academy Graduate<br />
+          Full-Stack Web Developer<br />
+          jeremyliss at gmail dot com
+        </p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Heading
