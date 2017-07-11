43802de8dd01e7441bf9480ca302a7a0335d59dd
Jeremy Liss <jeremyliss@gmail.com>
2017-07-11 22:37:32 +1200

    new layout with video background

--- components/App.js
+++ components/App.js
@@ -4,9 +4,15 @@ import Links from './Links'
 
 const App = (props) => {
   return (
-      <div className="homepage">
-        <Heading name={props.name} email={props.email} />
-        <Links links={props.links} />
+      <div className="heading">
+        <h1>JEREMY LISS</h1>
+        <p>Enspiral Dev Academy Graduate, Web Developer &tc.</p>
+        {props[0] && <h4><a href="#">Space Invaders</a></h4>}
+        {props[1] && <h4><a href='https://jeremy-liss.github.io/drum-machine/public/'> A Drum Sequencer</a></h4>}
+        {props[2] && <h4><a href='https://jeremy-liss.github.io/barbara-shop/public/'>Barbara Shop Quartet</a></h4>}
+        {props[3] && <h4><a href='https://hazardous-asteroids.herokuapp.com/'>Are There Any Hazerdous Asteroids<br/> Approaching Earth Today?</a></h4>}
+        {props[4] && <h4><a href='https://jeremy-liss.github.io/a-z/'>An A-Z Of Things In My Kitchen</a></h4>}
+        <p><a href='https://github.com/jeremy-liss/'>github</a> | email</p>
       </div>
     )
 }
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import state from './state'
 import App from './components/App'
+
+let state = [false, false, false, false, false]
+
+let count = 0
+
+const links = (num) => {
+  state[num] = true
+  count ++
+  render(state)
+}
+
+setInterval(()=>{
+  links(count)
+}, 700)
 
 const target = document.getElementById('root')
 
